e7603b2a964976a9b6c85941bdf20d7c139381c8
Peter Kondacs <kondacspeter@gmail.com>
2018-07-10 20:58:06 +0200

    Sending purchase email.

--- src/main/java/com/codecool/shitwish/service/EmailService.java
+++ src/main/java/com/codecool/shitwish/service/EmailService.java
@@ -1,5 +1,6 @@
 package com.codecool.shitwish.service;
 
+import com.codecool.shitwish.model.Order;
 import com.codecool.shitwish.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
@@ -36,6 +37,13 @@ public class EmailService {
         String recipient = user.getEmail();
         String subject = "Welcome to Shitwish.com";
         String content = mailContentBuilder.buildRegistrationMail(user);
+        prepareAndSend(recipient, subject, content);
+    }
+
+    public void sendPurchaseEmail(User user, Order order) {
+        String recipient = user.getEmail();
+        String subject = "Successful purchase, Order Id: " + order.getId();
+        String content = mailContentBuilder.buildPurchaseMail(user, order);
         prepareAndSend(recipient, subject, content);
     }
 }
