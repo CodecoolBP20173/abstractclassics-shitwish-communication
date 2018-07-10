eb45b94e0708300da16ed4ea9c01c7e19a047b51
Adam Grob <ad4mgr0b@gmail.com>
2018-07-10 16:05:41 +0200

    Implemented EmailService class.

--- src/main/java/com/codecool/shitwish/service/EmailService.java
+++ src/main/java/com/codecool/shitwish/service/EmailService.java
@@ -1,7 +1,44 @@
 package com.codecool.shitwish.service;
 
+import com.codecool.shitwish.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
+
+    private JavaMailSender mailSender;
+
+    private MailContentBuilder mailContentBuilder;
+
+    @Autowired
+    public EmailService(JavaMailSender mailSender, MailContentBuilder mailContentBuilder) {
+        this.mailSender = mailSender;
+        this.mailContentBuilder = mailContentBuilder;
+    }
+
+    public void prepareAndSend(String recipient, String content) {
+        MimeMessagePreparator messagePreparator = mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setTo(recipient);
+            messageHelper.setSubject("Sample mail subject");
+            messageHelper.setText(content, true);
+        };
+        try {
+            mailSender.send(messagePreparator);
+        } catch (MailException e) {
+            System.out.println("Something went wrong!");
+            e.printStackTrace();
+        }
+    }
+
+    public void sendRegistrationMail(User user) {
+        String recipient = user.getEmail();
+        String content = mailContentBuilder.buildRegistrationMail(user);
+        prepareAndSend(recipient, content);
+    }
 }
