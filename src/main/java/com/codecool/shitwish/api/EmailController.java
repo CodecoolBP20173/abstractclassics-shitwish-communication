cef1a9353347ac359e01e131e435073459914e29
Adam Grob <ad4mgr0b@gmail.com>
2018-07-10 16:05:22 +0200

    Implemented EmailController class.

--- src/main/java/com/codecool/shitwish/api/EmailController.java
+++ src/main/java/com/codecool/shitwish/api/EmailController.java
@@ -1,7 +1,28 @@
 package com.codecool.shitwish.api;
 
+import com.codecool.shitwish.model.User;
+import com.codecool.shitwish.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmailController {
+
+
+    private EmailService emailService;
+
+    @Autowired
+    public EmailController(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
+    @PostMapping(value = "/registration")
+    public HttpStatus sendRegistrationEmail(@RequestBody User user) {
+        emailService.sendRegistrationMail(user);
+        return HttpStatus.OK;
+    }
+
 }
